132919ccf7d8be39baeedfeebf24c85f943f321a
Imran Melikov <imran.melikov70@gmail.com>
2023-03-28 06:18:47 +0400

    searchactivity,adapter ended

--- app/src/main/java/com/imranmelikov/simpleinstagramclone/Activities/SearchActivity.kt
+++ app/src/main/java/com/imranmelikov/simpleinstagramclone/Activities/SearchActivity.kt
@@ -1,43 +1,94 @@
 package com.imranmelikov.simpleinstagramclone.Activities
 
-import android.location.GnssAntennaInfo.Listener
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.imranmelikov.simpleinstagramclone.Adapters.AdapterRV_notification
 import com.imranmelikov.simpleinstagramclone.Adapters.AdapterRV_search_activity
-import com.imranmelikov.simpleinstagramclone.Models.Post
 import com.imranmelikov.simpleinstagramclone.Models.User
-import com.imranmelikov.simpleinstagramclone.R
 import com.imranmelikov.simpleinstagramclone.databinding.ActivitySearchBinding
-import com.imranmelikov.simpleinstagramclone.databinding.ActivitySigninBinding
-import com.imranmelikov.simpleinstagramclone.databinding.FragmentNotificationsBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
-    private lateinit var mUser:ArrayList<User>
+    private lateinit var userarraylist:ArrayList<User>
     private lateinit var searchadapter:AdapterRV_search_activity
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivitySearchBinding.inflate(layoutInflater)
         val view=binding.root
         setContentView(view)
+        binding.searchBtn.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
 
-        mUser= ArrayList()
-        searchadapter= AdapterRV_search_activity(mUser)
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterlist(newText)
+                return true
+            }
+
+        })
+        auth= Firebase.auth
+        db= Firebase.firestore
+        getdata()
+
+        userarraylist= ArrayList()
+        searchadapter= AdapterRV_search_activity(userarraylist)
         binding.recyclerviewSearchActivity.layoutManager=LinearLayoutManager(this)
         binding.recyclerviewSearchActivity.adapter=searchadapter
 
+    }
+
+    fun getdata(){
+        db.collection("Posts").addSnapshotListener { value, error ->
+            if(error!=null){
+                Toast.makeText(this,error.localizedMessage, Toast.LENGTH_SHORT).show()
+            }else{
+                if(value!=null){
+                    if(!value.isEmpty){
+                       userarraylist.clear()
+                        val documents=value.documents
+                        for(document in documents){
+                            val downloadimage=document.get("downloadimage")
+                            val username=document.get("username")
+                            val profilename=document.get("profilename")
+                            if(profilename!=null){
+                                val user=User(profilename.toString(),downloadimage.toString())
+                                userarraylist.add(user)
+                            }else{
+                                val user=User(username.toString(),downloadimage.toString())
+                                userarraylist.add(user)
+                            }
+
+                        }
+                        searchadapter!!.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun filterlist(text: String?) {
+        var filteredlist:ArrayList<User>
+        filteredlist= ArrayList()
+        for(item in userarraylist){
+            if(item.username!!.toLowerCase().contains(text!!.toLowerCase()))
+                filteredlist.add(item)
+            if(filteredlist.isEmpty()){
+            }else{
+                searchadapter.filterList(filteredlist)
+            }
+
+        }
     }
 }
